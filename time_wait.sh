79be061edbb8ca4536cff59ad246aec6ba7acae3
Lincy Varghese <lincy.varghese@reancloud.com>
2017-06-13 12:37:40 +0530

    Updated with tcp_tw_recycle value
    Updated with tcp_tw_recycle value for recycling the data

--- time_wait.sh
+++ time_wait.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-# Rean Managed Service Script 
+#  
+# Lincy Varghese [5678945]
 
-
+# geting the time wait condtion set 
 $condtion=$(sudo netstat -ntp | grep -i "TIME_WAIT" | wc -l )
 
-if [ $condtion -ge 100 ];
-	Then
+if [ $condtion -ge 100 ]; Then
+  # adding the keyvalue for the tcp_te_reuse 
+  # default value is 0 changing to 1 to fluse the connections 
   sudo echo "1" > /proc/sys/net/ipv4/tcp_tw_reuse
-  sudo 
-else
-  
+  # Resatrting the service 
+  sudo service network restart
 fi
+printf "Value Added"
+
+if [ $condtion -ge 300 ]; Then
+  # adding the keyvalue for the tcp_te_reuse 
+  # default value is 0 changing to 1 to fluse the connections 
+  sudo echo "1" > /proc/sys/net/ipv4/tcp_tw_recycle
+  # Resatrting the service 
+  sudo service network restart
+fi
+
+printf "Value Add"
